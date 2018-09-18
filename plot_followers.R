732a8ce79a994b5f3c10bdcb8266f5fc97877947
Bernhard <berni98k@gmail.com>
2018-09-18 19:49:01 +0200

    custom run.py with config file and R script for plotting graphs

---
+++ plot_followers.R
@@ -0,0 +1,74 @@
+#!/usr/bin/env Rscript
+require(ggplot2)
+require(lubridate)
+
+folder='/var/www/html/followers/'
+
+readData <- function(path, colname){
+  data <- read.csv(path, header=FALSE, sep=" ", col.names=c('date','time', colname))
+  data <- within(data, {Date=as.POSIXct(paste(date, time))})
+  data <- subset(data, select=c('Date', colname))
+  return(data)
+}
+
+#read and format the data
+followerData <- readData("./logs/***REMOVED***/followerNum.txt", 'Followers')
+followingData <- readData("./logs/***REMOVED***/followingNum.txt", 'Following')
+
+created_time = format(max(followerData$Date))
+
+# plot total followers over time
+p1 <- ggplot(followerData, aes(x=Date, y=Followers)) + geom_line()
+p1 <- p1 + geom_line(data=followingData, aes(x=Date, y=Following), color='gray')
+p1 <- p1 + ggtitle(paste("Total Followers over Time\nCreated:", created_time))
+filename = paste(folder,'all_time.png', sep='')
+ggsave(filename, plot=p1)
+#plot(p1)
+
+# plot followers over last 30 days
+cutoff = max(followerData$Date) - days(30)
+p2 <- ggplot(followerData[followerData$Date >= cutoff,], aes(x=Date, y=Followers)) + geom_line()
+p2 <- p2 + geom_line(data=followingData[followingData$Date >= cutoff,], aes(x=Date, y=Following), color='gray')
+p2 <- p2 + ggtitle(paste("Followers over the last 30 Days\nCreated:", created_time))
+filename = paste(folder,'last_month.png', sep='')
+ggsave(filename, plot=p2)
+#plot(p2)
+
+xvals = as.numeric(readLines(paste(folder,'regression-days', sep='')))
+#xvals = c(1,3)
+for(x in xvals){
+  interval = days(x)
+  # plot a x day linear regression
+  cutoff = max(followerData$Date) - interval
+  current_data = followerData[followerData$Date > cutoff,]
+  linear <- lm(Followers ~ Date, current_data)
+  current_data$fit = fitted(linear)
+  pred = data.frame(Date=seq(max(followerData$Date), max(followerData$Date) + interval, length = length(current_data$Date)))
+  pred$Followers = predict(linear, newdata = pred)
+
+  p2 <- ggplot()
+  p2 <- p2 + geom_point(aes(x=Date, y=Followers), current_data)
+  p2 <- p2 + geom_line(aes(x=Date, y=fit), current_data)
+  p2 <- p2 + geom_line(aes(x=Date, y=Followers), pred, linetype="dashed")
+  p2 <- p2 + ggtitle(paste(x,'day prediction using linear regression over the last',x,'days\nCreated:', created_time))
+  filename = paste(folder,x,'-day-regression.png', sep='')
+  ggsave(filename, plot=p2)
+
+  # plot the last x day linear regression and compare it to current data
+  midpoint = max(followerData$Date) - interval
+  cutoff = midpoint - interval
+  old_data = followerData[followerData$Date >= cutoff & followerData$Date <= midpoint,]
+  new_data = followerData[followerData$Date >= midpoint,]
+  linear <- lm(Followers ~ Date, old_data)
+  old_data$fit = fitted(linear)
+  new_data$fit = predict(linear, newdata = new_data)
+
+  p3 <- ggplot()
+  p3 <- p3 + geom_point(aes(x=Date, y=Followers), new_data, col="dark gray")
+  p3 <- p3 + geom_point(aes(x=Date, y=Followers), old_data)
+  p3 <- p3 + geom_line(aes(x=Date, y=fit), old_data)
+  p3 <- p3 + geom_line(aes(x=Date, y=fit), new_data, linetype="dashed")
+  p3 <- p3 + ggtitle(paste('Last',x,'day prediction compared to the data of the last',x,'days\nCreated:', created_time))
+  filename = paste(folder,x,'-day-regression-prev.png', sep='')
+  ggsave(filename, plot=p3)
+}
---
+++ run.py
@@ -0,0 +1,115 @@
+import getopt
+import json
+import os
+import subprocess
+import sys
+import time
+from tempfile import gettempdir
+
+from selenium.common.exceptions import NoSuchElementException
+
+from instapy import InstaPy
+
+# set headless_browser=True if you want to run InstaPy on a server
+
+# set these in instapy/settings.py if you're locating the
+# library in the /usr/lib/pythonX.X/ directory:
+#   Settings.database_location = '/path/to/instapy.db'
+#   Settings.chromedriver_location = '/path/to/chromedriver'
+
+
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, "s")
+    except getopt.GetoptError:
+        print('run.py [-s]')
+        sys.exit(2)
+
+    silent = False
+    for opt, arg in opts:
+        if opt == '-s':
+            silent = True
+
+    with open('config.json') as f:
+        config = json.load(f)
+
+    session = InstaPy(
+        username=config['username'],
+        password=config['password'],
+        headless_browser=True,
+        show_logs=not silent,
+        multi_logs=True
+    )
+
+    try:
+        session.login()
+
+        ##########
+        # settings
+
+        session.set_relationship_bounds(
+            enabled=True,
+            potency_ratio=-0.9,
+            delimit_by_numbers=True,
+            max_followers=2000,
+            max_following=1000,
+            min_followers=5,
+            min_following=10,
+        )
+
+        session.set_dont_include(config['dont_unfollow'])
+
+        session.set_do_like(
+            enabled=True,
+            percentage=30,
+        )
+
+        session.set_user_interact(
+            amount=5,
+            randomize=False,
+        )
+
+        ##########
+        # do stuff
+
+        # update the follower graph
+        try:
+            subprocess.call("./plot_followers.R", stderr=subprocess.DEVNULL)
+        except Exception as e:
+            print('warning: graph not updated')
+            print(e)
+
+        session.follow_user_followers(
+            config['rolemodels'],
+            amount=20,
+            randomize=True,
+            interact=True,
+        )
+
+        session.set_dont_unfollow_active_users(enabled=True, posts=5)
+        session.unfollow_users(
+            amount=100,
+            #nonFollowers=True,
+            allFollowing=True,
+            unfollow_after=3*24*60*60,
+            sleep_delay=655,
+        )
+
+    except Exception as exc:
+        # if changes to IG layout, upload the file to help us locate the change
+        if isinstance(exc, NoSuchElementException):
+            file_path = os.path.join(gettempdir(), '{}.html'.format(time.strftime('%Y%m%d-%H%M%S')))
+            with open(file_path, 'wb') as fp:
+                fp.write(session.browser.page_source.encode('utf8'))
+            print('{0}\nIf raising an issue, please also upload the file located at:\n{1}\n{0}'.format(
+                '*' * 70, file_path))
+        # full stacktrace when raising Github issue
+        raise
+
+    finally:
+        # end the bot session
+        session.end()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
